85a8b400201c5aae767323df8392c21573d7f9ee
isnendyankp <isnendyankp@gmail.com>
2024-01-03 16:56:54 +0700

    cr8 PublicLayout component

---
+++ src/layouts/PublicLayout.tsx
@@ -0,0 +1,12 @@
+const PublicLayout = () => {
+  return (
+    <div>
+      <h1>Public Layout</h1>
+    </div>
+  );
+}
+
+export default PublicLayout;
+
+// Progress:
+// 1. cr8 PublicLayout component
