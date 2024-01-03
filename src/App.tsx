e89de6bdb9d7f378dbae3f759b0cad5a5b9fb57c
isnendyankp <isnendyankp@gmail.com>
2024-01-03 14:29:09 +0700

    import BrowserRouter, Routes, Route from react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css';
 import { HomeContainer } from './containers';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
@@ -15,26 +16,4 @@ export default App;
 // 1. cr8 App component
 // 2. import HomeContainer component
 // 3. render with pass in HomeContainer component
-
-// Cr8 simple form with add onSubmit to controlling input text & button submit
-/* 
-   <>
-     <form onSubmit={() => console.log('asd')}>
-      <input type="text" onChange={() => console.log('asd')} />
-      <input type="text" onChange={() => console.log('asd')} />
-      <input type="text" onChange={() => console.log('asd')} />
-      <input type="text" onChange={() => console.log('asd')} />
-      <input type="text" onChange={() => console.log('asd')} />
-      <button type="submit">Submit</button>
-    </form>
-  </>
-*/
-
-// test active tailwind using bg-red-500
-/*
-  return (
-    <div className='bg-red-500'>
-      check active tailwind
-    </div>
-  );
-*/
+// 4. import BrowserRouter, Routes, Route from react-router-dom
