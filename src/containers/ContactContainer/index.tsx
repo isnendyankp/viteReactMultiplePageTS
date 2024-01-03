0956481a9dc1fbebfc92605d8eec941bfc2735c6
isnendyankp <isnendyankp@gmail.com>
2024-01-03 14:27:16 +0700

    cr8 ContactContainer component

---
+++ src/containers/ContactContainer/index.tsx
@@ -0,0 +1,12 @@
+const ContactContainer = () => {
+  return (
+    <div>
+      <h1>Halaman Contact</h1>
+    </div>
+  );
+};
+
+export default ContactContainer;
+
+// Progress:
+// 1. cr8 ContactContainer component
