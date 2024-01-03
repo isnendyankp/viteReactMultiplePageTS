51437296b2ed4ae5d2877fb7c5d5e9ccb14313f8
isnendyankp <isnendyankp@gmail.com>
2024-01-03 18:13:31 +0700

    remove Navbar cz already use nested route with element PublicLayout component

--- src/containers/ContactContainer/index.tsx
+++ src/containers/ContactContainer/index.tsx
@@ -1,9 +1,6 @@
-import { Navbar } from "../../components";
-
 const ContactContainer = () => {
   return (
     <div>
-      <Navbar />
       <h1>Halaman Contact</h1>
     </div>
   );
@@ -14,3 +11,4 @@ export default ContactContainer;
 // Progress:
 // 1. cr8 ContactContainer component
 // 2. import Navbar & render @ContactContainer component
+// 3. remove Navbar cz already use nested route with element PublicLayout component
