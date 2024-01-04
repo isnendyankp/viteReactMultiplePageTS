531587cc9d8e22cdbfaeadb2430722abc65a4acc
isnendyankp <isnendyankp@gmail.com>
2024-01-04 17:33:11 +0700

    change contactContainer into listContainer

---
+++ src/containers/ListContainer/index.tsx
@@ -0,0 +1,21 @@
+/*
+  create code list page using react typescript  with:
+  1. category list form:
+  - user able to see list of category
+  - user able to save category
+  - user able to update category
+  - user able to delete category
+  2. make sure all form catergory have validation
+*/
+
+const ListContainer = () => {
+  return (
+    <div>
+      <h1>Halaman List</h1>
+    </div>
+  );
+};
+
+export default ListContainer;
+
+
