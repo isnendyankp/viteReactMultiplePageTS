4b9a9a7a5bd4edb85794575d71b6961eac9c7d80
isnendyankp <isnendyankp@gmail.com>
2024-01-04 17:38:02 +0700

    add axios & fix navbar

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import PublicLayout from './layouts/PublicLayout';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 function App() {
+
   const router = createBrowserRouter([
     {
       element: <PublicLayout />,
@@ -18,7 +19,9 @@ function App() {
 
   return (
     <div className="app">
+
       <RouterProvider router={router} />
+      
     </div>
   );
 }
@@ -40,3 +43,20 @@ export default App;
 // 12. render with cr8 route & pass in ContactContainer component
 // 13. import PublicLayout component
 // 14. add nested route with element PublicLayout component
+
+
+// Explanation:
+// Route version 5
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// {
+//   /* <BrowserRouter>
+//         <Routes>
+//           <Route element={<PublicLayout />}>
+//             <Route path="/" element={<HomeContainer />} />
+//             <Route path="/about" element={<AboutContainer />} />
+//             <Route path="/contact" element={<ContactContainer />} />
+//           </Route>
+//           <Route path="*" element={<h1>404</h1>} />
+//         </Routes>
+//       </BrowserRouter> */
+// }
--- src/containers/index.ts
+++ src/containers/index.ts
@@ -2,6 +2,7 @@ export { default as HomeContainer } from './HomeContainer';
 export { default as LoginContainer } from './LoginContainer';
 export { default as ListContainer } from './ListContainer';
 
+
 // Progress:
 // 1. Create file distributor src/containers/index.ts
 // 2. Add HomeContainer for container file distributor  @src/containers/index.ts
