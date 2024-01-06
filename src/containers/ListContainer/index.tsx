642d6e2139362c3bba0b1c9c2f9395e5bed35c81
isnendyankp <isnendyankp@gmail.com>
2024-01-06 13:18:48 +0700

    Fix ListContainer

--- src/containers/ListContainer/index.tsx
+++ src/containers/ListContainer/index.tsx
@@ -1,21 +1,90 @@
-/*
-  create code list page using react typescript  with:
-  1. category list form:
-  - user able to see list of category
-  - user able to save category
-  - user able to update category
-  - user able to delete category
-  2. make sure all form catergory have validation
-*/
+import React, { useEffect, useState } from 'react';
+import { deleteCategory, getCategories } from '../../api/categoryApi';
+import { CategoryData } from '../../interfaces/Category';
 
-const ListContainer = () => {
+const ListContainer: React.FC = () => {
+  const token = localStorage.getItem('token') ?? '';
+  const [categories, setCategories] = useState<CategoryData[]>([]);
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const response = await getCategories(token);
+        setCategories(response.data.data);
+
+        console.log('Dapet bang !');
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchCategories();
+  }, [token]);
+
+  const handleEdit = (
+    categoryId: string,
+    categoryName: string,
+    categoryStatus: boolean
+  ) => {
+    console.log(`edit category with ID: ${categoryId}`);
+    console.log(categoryName);
+    console.log(categoryStatus);
+  };
+
+  const handleDelete = async (categoryId: string) => {
+    try {
+      await deleteCategory(categoryId, token);
+      const updatedCategories = categories.filter(
+        (category) => category.id !== categoryId
+      );
+      setCategories(updatedCategories);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div>
-      <h1>Halaman List</h1>
+      <h2 className="text-xl font-semibold text-center">Category List</h2>
+      <table className="table-auto w-full">
+        <thead>
+          <tr>
+            <th className="px-4 py-2">ID</th>
+            <th className="px-4 py-2">Name</th>
+            <th className="px-4 py-2">Status</th>
+            <th className="px-4 py-2">Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {categories.map((category) => (
+            <tr key={category.id}>
+              <td className="border px-4 py-2">{category.id}</td>
+              <td className="border px-4 py-2">{category.name}</td>
+              <td className="border px-4 py-2">
+                {category.is_active ? 'Active' : 'Deactive'}
+              </td>
+              <td className="border px-4 py-2">
+                <button
+                  onClick={() =>
+                    handleEdit(category.id, category.name, category.is_active)
+                  }
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                >
+                  Edit
+                </button>
+                <button
+                  onClick={() => handleDelete(category.id)}
+                  className="ml-2 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+          <td></td>
+        </tbody>
+      </table>
     </div>
   );
 };
 
 export default ListContainer;
-
-
