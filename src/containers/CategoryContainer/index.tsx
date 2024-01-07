b781da2454c165b2267720a81af5ad917e008d64
isnendyankp <isnendyankp@gmail.com>
2024-01-07 17:07:45 +0700

    cr8 Category Container

---
+++ src/containers/CategoryContainer/index.tsx
@@ -0,0 +1,16 @@
+
+
+import ListCategory from '../Categories/ListCategory';
+import CreateCategory from '../Categories/CreateCategory';
+
+const CategoryContainer = () => {
+  return (
+    <div>
+      <CreateCategory/>
+      <br/>
+      <ListCategory />
+    </div>
+  )
+}
+
+export default CategoryContainer
