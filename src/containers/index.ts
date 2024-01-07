bf9eea2ca4706dcd17ffcd0d6d2ea686079068ca
isnendyankp <isnendyankp@gmail.com>
2024-01-07 18:25:43 +0700

    Add base createcategory

--- src/containers/index.ts
+++ src/containers/index.ts
@@ -1,6 +1,7 @@
 export { default as HomeContainer } from './HomeContainer';
 export { default as LoginContainer } from './LoginContainer';
 export { default as ListContainer } from './ListContainer';
+export { default as CreateCategoryContainer } from './CreateCategoryContainer';
 
 
 // Progress:
