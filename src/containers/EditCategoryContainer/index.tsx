9f6ee400697f2c88bf91357fab5992107f875b56
isnendyankp <isnendyankp@gmail.com>
2024-01-16 16:24:48 +0700

    cr8 base EditCategoryContainer

---
+++ src/containers/EditCategoryContainer/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const EditCategory = () => {
+  return (
+    <div>index</div>
+  )
+}
+
+export default EditCategory
--- src/containers/index.ts
+++ src/containers/index.ts
@@ -2,6 +2,7 @@ export { default as HomeContainer } from './HomeContainer';
 export { default as LoginContainer } from './LoginContainer';
 export { default as ListContainer } from './ListContainer';
 export { default as CreateCategoryContainer } from './CreateCategoryContainer';
+export { default as EditCategoryContainer } from './EditCategoryContainer';
 
 
 // Progress:
