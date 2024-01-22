6738996462e367e413c81334295c2d1496fb4633
isnendyankp <isnendyankp@gmail.com>
2024-01-22 20:16:09 +0700

    Add interface UpdateCategoryFormProps

--- src/components/UpdateForm/index.tsx
+++ src/components/UpdateForm/index.tsx
@@ -1,4 +1,19 @@
 // Purpose: Provide a form for updating a list category.
+
+
+// interface
+interface UpdateCategoryFormProps {
+  onUpdateSuccess: () => void;
+  onCancel: () => void;
+  category: {
+    id: string;
+    name: string;
+    is_active: boolean;
+  };
+}
+
+
+// component
 const UpdateForm = () => {
   return (
     <div>index</div>
